e2202d66a9ec3b4625c070564869b77b70d18e41
YipengLi李依朋 <65066990+liyipeng-moon@users.noreply.github.com>
2023-02-20 12:35:28 +0800

    change location

--- fNs/fN_change_gui_color.m
+++ fNs/fN_change_gui_color.m
@@ -1,15 +1,15 @@
-function app = fN_change_gui_color(app)
-global BAM_config BAM_data;
-
+function app = fN_change_gui_color(app,BAM_config,BAM_data)
 %% about saving
 if(BAM_config.is_saving)
     app.StartOnlineSavingButton.FontColor = BAM_config.colormap.white;
     app.StopOnlineSavingButton.FontColor = BAM_config.colormap.black;
     app.SavingLamp.Color=BAM_config.colormap.green;
+    app.SavingLampLabel.Text='Saving';
 else
     app.SavingLamp.Color=BAM_config.colormap.red;
     app.StartOnlineSavingButton.FontColor = BAM_config.colormap.black;
     app.StopOnlineSavingButton.FontColor = BAM_config.colormap.white;
+    app.SavingLampLabel.Text='Stopped';
 end
 
 end
