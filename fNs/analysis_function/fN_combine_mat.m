a3874741e178c1e2c48de3d98b968c92ed563bb6
YipengLi李依朋 <65066990+liyipeng-moon@users.noreply.github.com>
2023-02-25 22:26:10 +0800

    adding combine data

--- fNs/analysis_function/fN_combine_mat.m
+++ fNs/analysis_function/fN_combine_mat.m
@@ -1,19 +1,43 @@
 function [BAM_analysis, app]=fN_combine_mat(today_name,root_dir, app)
-    BAM_analysis.folder_today = [root_dir, '/data/',today_name, '*'];
+    BAM_analysis.folder_today = [root_dir, 'data\',today_name, '*'];
     BAM_analysis.all_session = dir(BAM_analysis.folder_today);
-    
+
     for ses = 1:length(BAM_analysis.all_session)
-        ses_folder = [BAM_analysis.all_session(ses).folder, '\', BAM_analysis.all_session(ses).name];
+        ses_folder = fullfile(BAM_analysis.all_session(ses).folder, BAM_analysis.all_session(ses).name);
         all_uncheck = dir([ses_folder,'\uncheck*']);
     
         if(~isempty(all_uncheck))
             first_data = load([ses_folder,'\',all_uncheck(1).name]).BAM_data;
             BAM_analysis.dataset{ses} = [num2str(ses),'-',first_data.Dataset];
+        else
+            % which means there is no un-processed data or there is no data
+            % yet, we can just jump over this session
+            continue
         end
-        % combine..
+
+        % load combined data
+        if(isempty(dir(fullfile(ses_folder, 'combined_data.mat'))))
+            combined_data = struct;
+            save(fullfile(ses_folder, 'combined_data.mat'), "combined_data")
+        else
+            combined_data = load(fullfile(ses_folder, 'combined_data.mat')).combined_data;
+        end
+
+        for mat_idx = 1:length(all_uncheck)
+            temp_bam_data = load(fullfile(ses_folder, all_uncheck(mat_idx).name)).BAM_data;
+            
+            % adding temp mat to combined mat
+            combined_data = fN_prep_and_stack_data(combined_data, temp_bam_data);
+
+
+            file_name = all_uncheck(mat_idx).name;
+            eval(['!rename',' , ', fullfile(ses_folder, all_uncheck(mat_idx).name) ' ,' file_name(3:end) ])
+        end
+    
+    
+    
     end
 
     app.SelectSession.Items = BAM_analysis.dataset;
-    app.SelectSession.Value = BAM_analysis.dataset{ses};
     pause(0.05)
 end
---
+++ fNs/analysis_function/fN_prep_and_stack_data.m
@@ -0,0 +1,3 @@
+function combined_data = fN_prep_and_stack_data(combined_data, temp_bam_data)
+
+end
